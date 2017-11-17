f87146391f97b6c4c0407f3bcd74c3a00d97ed2a
David Pickart <davidstoreypickart@gmail.com>
2017-11-17 13:30:10 -0600

    Use shared lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,3 @@
 module.exports = {
-  'env': {
-    'node': true,
-    'es6': true,
-    'jest': true,
-  },
-  'parser': 'babel-eslint',
-  'ecmaFeatures': {
-    'experimentalObjectRestSpread': true,
-  },
-  'extends': [
-    'eslint:recommended',
-  ],
-  'rules': {
-    'no-console': [
-      'warn',
-    ],
-    'no-unused-vars': [
-      'warn',
-    ],
-  },
+  'extends': '@launchpadlab/eslint-config/es6'
 }
