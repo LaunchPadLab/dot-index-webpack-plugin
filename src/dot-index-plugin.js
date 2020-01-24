24eae88db18e95b957e41b10c9857ef779a3b876
David Pickart <davidstoreypickart@gmail.com>
2020-01-24 11:08:38 -0600

    Replace `watch` module with webpack functionality (#12)

--- src/dot-index-plugin.js
+++ src/dot-index-plugin.js
@@ -1,34 +1,32 @@
-const path  = require('path')
-const { createMonitor } = require('watch')
 const createDotIndexFiles = require('./utils/create-dot-index-files')
 
-function DotIndexPlugin (options={}) {
+function DotIndexPlugin(options = {}) {
   this.options = options
   this.initialized = false
 }
 
-DotIndexPlugin.prototype.apply = function (compiler) {
+// In webpack v4, contextDependencies is a Set
+function addContextDependency(compilation, dependency) {
+  compilation.contextDependencies.add(dependency)
+}
+
+DotIndexPlugin.prototype.apply = function(compiler) {
   const { path: rootPath, ...options } = this.options
   if (!rootPath) throw new Error('DotIndexPlugin requires "path" option')
-  function recompile () {
-    return compiler.run(() => {})
+  // Add root path to contextDependencies
+  // This will make webpack watch the entire directory
+  compiler.hooks.afterEmit.tapAsync('DotIndexPlugin', (compilation, cb) => {
+    addContextDependency(compilation, rootPath)
+    cb()
+  })
+  // Before compilation, build dot index files
+  function onRun(_compilation, cb) {
+    createDotIndexFiles(rootPath, options)
+    cb()
   }
-  compiler.hooks.watchRun.tapAsync('DotIndexPlugin', (compilation, callback) => {
-    createDotIndexFiles(rootPath, options)
-    // Only initialize once
-    if (this.initialized) return callback()
-    createMonitor(rootPath, monitor => {
-      monitor.files[path.join(rootPath, '*.js')]
-      monitor.on('created', recompile)
-      monitor.on('removed', recompile)
-      this.initialized = true
-      callback()
-    })
-  })
-  compiler.hooks.run.tapAsync('DotIndexPlugin', (compilation, callback) => {
-    createDotIndexFiles(rootPath, options)
-    callback()
-  })
+  // Hook into both normal and watch mode
+  compiler.hooks.run.tapAsync('DotIndexPlugin', onRun)
+  compiler.hooks.watchRun.tapAsync('DotIndexPlugin', onRun)
 }
 
 module.exports = DotIndexPlugin
